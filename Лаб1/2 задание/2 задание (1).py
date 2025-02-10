2e9aced5464c960c8d6da73f0bf3f71dfecc566a
PetryakovALeks <petryakov.sanya2018@yandex.ru>
2025-02-10 22:15:27 +0300

    3 задание(10)

--- Лаб1/2 задание/2 задание (1).py
+++ Лаб1/2 задание/2 задание (1).py
@@ -1,11 +1,12 @@
+#№2
 def sort(s):
     lovr_ = [char for char in s if char.islower()]  # Фильтруем строчные символы
     
-    # Если нет строчных символов, возвращаем True (пустая последовательность считается упорядоченной)
+   
     if not lovr_:
         return True
     
-    # Проверяем, отсортированы ли символы по возрастанию
+
     for i in range(1, len(lovr_)):
         if lovr_[i] < lovr_[i - 1]:
             return False
@@ -13,11 +14,3 @@ def sort(s):
 
 # Примеры использования
 print(sort("ABCDEfg"))  # True (строчные символы "f", "g" упорядочены)
-
-def count_a(s):
-    return s.count('А')  # Считаем количество букв "А"
-
-# Пример использования
-text = "АБВГААДЕА"
-print(f"Количество букв 'А': {count_a(text)}")
-  
